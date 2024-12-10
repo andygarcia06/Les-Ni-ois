71de7a99e635a5e5eb0817d37b2eb8abc14df191
Andy Garcia <andygarcia@live.fr>
2024-12-10 10:02:38 +0100

    Add Procfile and specify buildpack

---
+++ .history/assets/js/manifesto-functions_20241209193400.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Swiper pour desktop
+    const desktopSwiper = new Swiper('.manifesto-swiper-desktop', {
+        loop: true,
+        autoplay: {
+            delay: 5000, // 5 secondes
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        keyboard: {
+            enabled: true, // Navigation clavier activée
+            onlyInViewport: true, // Active uniquement quand visible
+        },
+    });
+
+    // Swiper pour mobile
+    const mobileSwiper = new Swiper('.manifesto-swiper-mobile', {
+        loop: true,
+        autoplay: {
+            delay: 5000, // 5 secondes
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        keyboard: {
+            enabled: true, // Navigation clavier activée
+            onlyInViewport: true, // Active uniquement quand visible
+        },
+    });
+});
